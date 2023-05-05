3d299b36d47a304aa735cd9daf2c10f792278e8a
OlmoDalco <yannik.hake@gmail.com>
2023-05-05 17:50:57 +0200

    adding languages

--- inlang.config.js
+++ inlang.config.js
@@ -1,14 +1,15 @@
+/**
+ * @type { import("@inlang/core/config").DefineConfig }
+ */
 export async function defineConfig(env) {
-    const { default: pluginJson } = await env.$import(
-        'https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js'
-    );
-
-    const { default: standardLintRules } = await env.$import(
-        'https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js'
-    );
+    const { default: jsonPlugin } = await env.$import(
+        "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js",
+    )
 
     return {
-        referenceLanguage: 'en',
-        plugins: [pluginJson({ pathPattern: './resources/{language}.json' }), standardLintRules()]
-    };
+        referenceLanguage: "en",
+        plugins: [
+            jsonPlugin({ pathPattern: "./languages/{language}.json" }),
+        ],
+    }
 }
